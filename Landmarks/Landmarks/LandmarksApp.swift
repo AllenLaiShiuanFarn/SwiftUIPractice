8d1fcded0ce4b9ee2e0069fa905e5b9fbb297a8c
Allen Lai <allenlai@kkstream.com>
2021-12-20 16:39:52 +0800

    feat: init Landmarks project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by allenlai on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
