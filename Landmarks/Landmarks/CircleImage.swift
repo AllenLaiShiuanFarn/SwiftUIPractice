9d070312b603826c00e6ec690746b028989312db
Allen Lai <allenlai@kkstream.com>
2021-12-20 19:03:35 +0800

    feat: Section 4 Create a Custom Image View

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,31 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by allenlai on 2021/12/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image(.Assets.turtlerock)
+            .clipShape(Circle())    // vs. iOS 15.0+: mask(alignment:_:) / iOS 13.0-15.2: mask(_:)
+            .overlay {              // vs. ZStack
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+
+extension String {
+    struct Assets {
+        static let turtlerock = "turtlerock"
+    }
+}
