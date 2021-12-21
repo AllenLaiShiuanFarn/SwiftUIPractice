b86fb0e4652caed66a5d7a0ed576b8882298de61
Allen Lai <allenlai@kkstream.com>
2021-12-21 11:39:00 +0800

    feat: Section 7 Pass Data into Child Views

--- Landmarks/Landmarks/Views/CircleImage.swift
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image(.Assets.turtlerock)
+        image
             .clipShape(Circle())    // vs. iOS 15.0+: mask(alignment:_:) / iOS 13.0-15.2: mask(_:)
             .overlay {              // vs. ZStack
                 Circle().stroke(.white, lineWidth: 4)
@@ -20,7 +22,7 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image(.Assets.turtlerock))
     }
 }
 
